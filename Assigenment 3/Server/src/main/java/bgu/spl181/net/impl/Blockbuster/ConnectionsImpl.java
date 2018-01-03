f16741a9fa4944ad1afcde82be8bcc56a271795a
Maor Assayag <maorassayag16@gmail.com>
2018-01-03 19:18:48 +0200

    finished

--- Assigenment 3/Server/src/main/java/bgu/spl181/net/impl/Blockbuster/ConnectionsImpl.java
+++ Assigenment 3/Server/src/main/java/bgu/spl181/net/impl/Blockbuster/ConnectionsImpl.java
@@ -1,5 +1,6 @@
 package bgu.spl181.net.impl.Blockbuster;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import bgu.spl181.net.api.bidi.Connections;
 import bgu.spl181.net.srv.ConnectionHandler;
@@ -7,29 +8,38 @@ import bgu.spl181.net.srv.ConnectionHandler;
 public class ConnectionsImpl <T> implements Connections<T> {
 	
 	private HashMap<Integer, ConnectionHandler<T>> activeClients = new HashMap<Integer, ConnectionHandler<T>>();
+	int counter = -1;
 
-	public void UpdateConnections(int connectionId ,ConnectionHandler<T> connectionHandler ){
-	    if(!activeClients.containsKey(connectionId))
-	        activeClients.put(connectionId,connectionHandler);
+	public int addConnection(ConnectionHandler<T> connectionHandler){  
+	   activeClients.put(counter++,connectionHandler);
+	   return counter;
     }
 
 	@Override
     public boolean send(int connectionId, T msg) {
-        return false;
+		if (!this.activeClients.containsKey(connectionId))
+			return false;
+		try {
+			this.activeClients.get(connectionId).send(msg);
+			return true;
+		} 
+		catch (Exception e) {
+			return false;
+		}
     }
 
     @Override
     public void broadcast(T msg) {
-
+    	for(int id:this.activeClients.keySet()) {
+    		this.activeClients.get(id).send(msg);
+    	}
     }
 
     @Override
     public void disconnect(int connectionId) {
-
-    }
-    
-    public void update() {
-		// TODO Auto-generated method stub
-	}
+    	if(this.activeClients.containsKey(connectionId)) {
+    		this.activeClients.remove(connectionId);
+    	}
+    } 
  
 }
