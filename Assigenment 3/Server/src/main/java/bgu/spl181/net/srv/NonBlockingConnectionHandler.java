1d2bc1476777bfbada1868b2ec7b93bb75ec0dcf
refael <rapooil120@gmail.com>
2018-01-04 16:45:38 +0200

    04.01/16:45

--- Assigenment 3/Server/src/main/java/bgu/spl181/net/srv/NonBlockingConnectionHandler.java
+++ Assigenment 3/Server/src/main/java/bgu/spl181/net/srv/NonBlockingConnectionHandler.java
@@ -120,8 +120,8 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
     }
 	@Override
 	public void send(T msg) {
-		// TODO Auto-generated method stub
-		
+        writeQueue.add(ByteBuffer.wrap(encdec.encode(msg)));
+        reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE)	;
 	}
 
 }
