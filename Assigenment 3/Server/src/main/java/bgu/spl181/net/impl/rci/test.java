15f1e263662df24531361e8c2319351eb00c3a7a
Maor Assayag <maorassayag16@gmail.com>
2018-01-06 22:46:20 +0200

    update

---
+++ Assigenment 3/Server/src/main/java/bgu/spl181/net/impl/rci/test.java
@@ -0,0 +1,40 @@
+package bgu.spl181.net.impl.rci;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+
+public class test {
+	String id;
+	int num;
+	String command;
+	
+	public test(String id, int num, String command) {
+		this.id = id;
+		this.num = num;
+		this.command = command;
+	}
+	
+public static void main(String[] args) throws JsonIOException, IOException {
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	test obj = new test("maor", 14, "try");
+	
+	// 1. Java object to JSON, and save into a file
+	try {
+		String json = gson.toJson(obj);
+		gson.toJson(json, new FileWriter("C:\\Users\\MaorA\\Desktop\\try.json"));
+	} catch (Exception e) {
+		System.out.println("fail");	
+}
+	
+
+	// 2. Java object to JSON, and assign to a String
+	String jsonInString = gson.toJson(obj);
+	System.out.println(jsonInString);
+
+}
+}
