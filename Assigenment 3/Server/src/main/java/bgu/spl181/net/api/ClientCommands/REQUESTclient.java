d4a4bccb946e03fc7b0740d1829cd4c881950e56
refael <rapooil120@gmail.com>
2018-01-08 13:33:56 +0200

    13:33

--- Assigenment 3/Server/src/main/java/bgu/spl181/net/api/ClientCommands/REQUESTclient.java
+++ Assigenment 3/Server/src/main/java/bgu/spl181/net/api/ClientCommands/REQUESTclient.java
@@ -4,6 +4,7 @@ import bgu.spl181.net.api.ServerCommands.ACKmsg;
 import bgu.spl181.net.api.ServerCommands.ERRORmsg;
 import bgu.spl181.net.impl.Blockbuster.DataBaseHandler;
 import bgu.spl181.net.impl.Blockbuster.gsonimpl.UserJson;
+import bgu.spl181.net.impl.Blockbuster.gsonimpl.user;
 import bgu.spl181.net.impl.Blockbuster.gsonimpl.users;
 
 
@@ -26,9 +27,19 @@ public class REQUESTclient extends ClientCommandsAbstract {
                     if(Commands.length == 3 && Commands[2].equals("info")){
                         dataBaseHandler.getReadWriteLockUsers().readLock().lock();
                         users users = new UserJson(dataBaseHandler.getPathUsers()).getUsers();
-                        String 
-                        ans = new ACKmsg()
-                    }else
+                        String balance ="" + users.GetUser(ClieantName).getBalance();
+                        ans = new ACKmsg("balance" + balance).getMsg();
+                        dataBaseHandler.getReadWriteLockUsers().readLock().unlock();
+                    }else if(Commands.length == 3 && Commands[3].equals("add") && Commands.length == 4 ){
+                        dataBaseHandler.getReadWriteLockUsers().writeLock().lock();
+                        users users = new UserJson(dataBaseHandler.getPathUsers()).getUsers();
+                        user user =  users.GetUser(ClieantName);
+
+                        //String balance ="" + users.GetUser(ClieantName).getBalance();
+                        ans = new ACKmsg("balance" + balance).getMsg();
+                        dataBaseHandler.getReadWriteLockUsers().writeLock().unlock();
+                    }
+                    else
                         ans = new ERRORmsg("request" + Commands[2] +"failed").getMsg();
             }
         }else{
--- Assigenment 3/Server/src/main/java/bgu/spl181/net/impl/Blockbuster/gsonimpl/test.java
+++ Assigenment 3/Server/src/main/java/bgu/spl181/net/impl/Blockbuster/gsonimpl/test.java
@@ -5,28 +5,30 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class test  {
     public static void main(String[] args){
-        String path ="C:/Users/USER/Desktop/test/Users.json";
-        String [] con ={"a","c","f"};
-        movies movies = new movies();
-        movies.AddMovie(new movie(
-                "a",
-                "3",
-                con,
-                "3"
-        ));
-        movies.AddMovie(new movie(
-                "b",
-                "4",
-                con,
-                "5"
-        ));
-        movies.AddMovie(new movie(
-                "c",
-                "5",
-                con,
-                "4"
-        ));
-        users u =new UserJson(path).getUsers();
-        new UserJson("C:/Users/USER/Desktop/test/Userstest.json").UpdateUser(u);
+        String a = "545";
+        System.out.println(Integer.valueOf(a));
+//        String path ="C:/Users/USER/Desktop/test/Users.json";
+//        String [] con ={"a","c","f"};
+//        movies movies = new movies();
+//        movies.AddMovie(new movie(
+//                "a",
+//                "3",
+//                con,
+//                "3"
+//        ));
+//        movies.AddMovie(new movie(
+//                "b",
+//                "4",
+//                con,
+//                "5"
+//        ));
+//        movies.AddMovie(new movie(
+//                "c",
+//                "5",
+//                con,
+//                "4"
+//        ));
+//        users u =new UserJson(path).getUsers();
+//        new UserJson("C:/Users/USER/Desktop/test/Userstest.json").UpdateUser(u);
     }
 }
